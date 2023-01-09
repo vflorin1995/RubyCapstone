2638fcad1182b45a8c1f708496adafff38371383
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-09 13:53:56 +0100

    Add methods for Item class

--- item.rb
+++ item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 Class item
     attr_accessor: :id, :genre, :author, :label, :source, :publish_date, :archived
     def initialize (id = Random.rand(1..1000), genre, author, label, source, publish_date, archived: true)
@@ -8,5 +10,18 @@ Class item
         @source = source
         @publish_date = publish_date
         @archived = archived
+    end
+
+    def can_be_archived?
+        today_date = DateTime.now.strftime("%d/%m/")
+        year = DateTime.now.strftime("%y")
+        ref_date = today_date + (year.to_i - 10).to_s
+        publish_date > ref_date
+    end
+
+    def move_to_archive?
+        if can_be_archived?
+            @archived = true
+        end
     end
 end
