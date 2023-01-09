6df5e23ee6cb42f2d9b3e49987c7e709b9bb7022
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-09 17:51:11 +0430

    Update item.rb

--- item.rb
+++ item.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 require 'date'
+# Item class
+class Item
+  attr_accessor :id, :genre, :author, :label, :source, :publish_date, :archived
 
-Class item
-    attr_accessor: :id, :genre, :author, :label, :source, :publish_date, :archived
-    def initialize (id = Random.rand(1..1000), genre, author, label, source, publish_date, archived: true)
-        @id = id
-        @genre = genre
-        @author = author
-        @label = label
-        @source = source
-        @publish_date = publish_date
-        @archived = archived
-    end
+  def initialize(genre, author, label, source, publish_date)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @label = label
+    @source = source
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def can_be_archived?
-        today_date = DateTime.now.strftime("%d/%m/")
-        year = DateTime.now.strftime("%y")
-        ref_date = today_date + (year.to_i - 10).to_s
-        publish_date > ref_date
-    end
+  def can_be_archived?
+    today_date = DateTime.now.strftime('%d/%m/')
+    year = DateTime.now.strftime('%y')
+    ref_date = today_date + (year.to_i - 10).to_s
+    publish_date > ref_date
+  end
 
-    def move_to_archive?
-        if can_be_archived?
-            @archived = true
-        end
-    end
+  def move_to_archive?
+    return unless can_be_archived?
+
+    @archived = true
+  end
 end
