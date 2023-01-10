ed6118be7a1fe71fa5bc50c6ff5c34130e51e022
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-10 14:34:29 +0100

    Modify on_sportify variable

--- music_album.rb
+++ music_album.rb
@@ -2,7 +2,7 @@ require_relative 'item'
 
 # MusicAlbum class
 class MusicAlbum < Item
-  def initialize(on_sportify)
+  def initialize(on_sportify: false)
     super(genre, author, label, publish_date, archived: false)
     @on_sportify = on_sportify
   end
