2f1b73a74c36af8283e594ac2e6f7c963afbe174
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-10 22:45:20 +0100

    Add attr_accessor for publish_date

--- music_album.rb
+++ music_album.rb
@@ -1,16 +1,18 @@
-# frozen_string_literal: true
+require 'json'
 
 require_relative 'item'
 
 # MusicAlbum class
 class MusicAlbum < Item
-  def initialize(on_sportify: false)
-    super(genre, author, label, publish_date, archived: false)
+  attr_accessor :on_sportify, :publish_date
+
+  def initialize(on_sportify, id = Random.rand(1..1000))
+    super(id, publish_date)
     @on_sportify = on_sportify
   end
 
   # Check whether to be archived
   def can_be_archived?
-    @archived == true && @on_sportify == true
+   @archived && @on_sportify == true
   end
 end
