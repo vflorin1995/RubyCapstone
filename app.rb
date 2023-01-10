e7c87007022dc1f2a7cc7805eba49ac8dad2f1fa
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-11 00:02:12 +0100

    Add MusicAlbumHandler module

--- app.rb
+++ app.rb
@@ -4,8 +4,12 @@ require_relative 'book'
 require_relative 'label'
 require_relative 'genre'
 require_relative 'music_album'
+require_relative 'album_handler'
+
 
 class App
+  include MusicAlbumHandler
+
   def initialize
     @books = []
     @albums = []
@@ -83,52 +87,6 @@ class App
     else
       @labels.each { |label| puts "Label name #{label.title} of color #{label.color}" }
     end
-    menu
-  end
-
-  # Add Music Album
-  def add_a_music_album
-    puts "Is the album on Spotify? [y/n]"
-    input = gets.chomp.downcase
-    on_sportify = input == 'y'
-    puts "Publish date:"
-    publish_date = gets.chomp.to_s
-    album = MusicAlbum.new(on_sportify, publish_date)
-    @albums << album
-    puts "Would you want to add genre? (1): "
-    option = gets.chomp.to_i
-    if option == 1
-      puts "Enter a genre for the album (e.g 'Comedy', 'Thriller'): "
-      name = gets.chomp
-      genre = Genre.new(name)
-      @genres << genre
-    end
-    puts "Music album added successfully"
-    puts " "
-    menu
-  end
-
-  # List Music Album
-  def list_all_music_albums
-    if @albums.empty?
-      puts "\nThere are no music album available\n"
-      puts ''
-    else
-      @albums.each { |album| puts "on Sportify?: #{album.on_sportify}, Publish date: #{album.publish_date}" }
-    end
-    puts ' '
-    menu
-  end
-
-  # List Genres
-  def list_all_genres
-    if @genres.empty?
-      puts "\nThere are no genre available"
-         puts ''
-    else
-      @genres.each { |genre| puts "Name: #{genre.name}" }
-    end
-    puts ' '
     menu
   end
 end
