3d431c51b07c2e5ef6ce7e4511ee9d807c5f94ae
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-11 16:41:16 +0430

    Added Unit Test for Author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../author'
+require_relative '../item'
+
+describe Author do
+  context 'It should create author and add to items' do
+    author = Author.new('Faranosh', 'Amini')
+    it 'Creates a new author' do
+      expect(author).to be_an_instance_of Author 
+    end 
+    it 'Should return array of items' do
+      expect(author.item.class).to eq Array
+    end 
+    it 'Should return First Name' do
+      expect(author.first_name).to eq 'Faranosh'
+    end 
+    it 'Should return Last Name' do
+      expect(author.last_name).to eq 'Amini'
+    end 
+  end    
+end  
