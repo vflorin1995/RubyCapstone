5a2d2759acb9f952b5071548290bfdf95ceb6d3e
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-11 17:03:12 +0100

    Add schema for MusicAlbum and Genre

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -17,3 +17,22 @@ CREATE TABLE books
     genre_id INT,
     source_id INT
 );
+
+CREATE TABLE genre
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(100)
+    FOREIGN KEY(item_id) REFERENCES item_id(id)
+);
+
+CREATE TABLE music_album
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    publish_date DATE,
+    archived BOOLEAN DEFAULT false,
+    on_sportify BOOLEAN,
+    author_id INT,
+    label_id INT REFERENCES labels(id),
+    genre_id INT,
+    source_id INT
+);
