d60ccfec559a6e7cd56a1a087a90ff3bcafe278b
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-11 11:10:34 +0430

    Create game.rb

---
+++ game.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+require 'date'
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+    super(publish_date)
+  end
+  
+  private
+
+  def can_be_archived?
+  end
+end  
