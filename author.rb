9a9389b39c7f9eb101a7d09432c4b459b478dd5c
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-11 12:46:27 +0430

    Fix Linter errors

--- author.rb
+++ author.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
+# Author class
 class Author
   attr_accessor :first_name, :last_name, :items
-  
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
     @items = []
-  end 
-  
-  def add_items(given_Item)
-    @items << given_Item
-    given_Item.author(self)
   end
-end    
+
+  def add_items(given_item)
+    @items << given_item
+    given_item.author(self)
+  end
+end
